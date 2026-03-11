ac145d8a67f43c96db7c99bf54d2525b04f38ef2
Nazim Fataliev <nazim.rabota@bk.ru>
2026-03-11 12:04:32 +0300

    Варианты записи переменных

--- main.go
+++ main.go
@@ -6,9 +6,10 @@ import (
 )
 
 func main() {
-	var userHeight = 1.7
-	var userKG float64 = 78
-	var imt = userKG / math.Pow(userHeight, 2)
+	userHeight := 1.7 //если тип будет на вывод то короткая запись
+	var userKG float64
+	userKG = 78 //если мы не используем сразу переменную
+	imt := userKG / math.Pow(userHeight, 2)
 	fmt.Print(imt)
 
 }
