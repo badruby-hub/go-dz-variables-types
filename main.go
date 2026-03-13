8a45f2fa4371da5aa53e447ba4a3677204983114
Nazim Fataliev <nazim.rabota@bk.ru>
2026-03-13 12:36:57 +0300

    константы

--- main.go
+++ main.go
@@ -6,10 +6,11 @@ import (
 )
 
 func main() {
+	const imtPower = 2
 	userHeight := 1.7 //если тип будет на вывод то короткая запись
 	var userKG float64
 	userKG = 78 //если мы не используем сразу переменную
-	imt := userKG / math.Pow(userHeight, 2)
+	imt := userKG / math.Pow(userHeight, imtPower)
 	fmt.Print(imt)
 
 }
