c7ab409be7c39f8fcd5ebd85d192e8d44b0b1011
Nazim Fataliev <nazim.rabota@bk.ru>
2026-03-25 13:26:25 +0300

    считываем введенные данные от пользователя

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -5,8 +5,17 @@ import "fmt"
 func main() {
 	const usdEur = 1 * 0.9412
 	const usdRub = 1 * 92
-	const eurRub = usdEur * usdRub
-	fmt.Println("Курс 1$ USD ⇄ 0.9412€ EUR =", usdEur, "EUR")
-	fmt.Println("Курс 1$ USD ⇄ 92₽ RUB =", usdRub, "RUB")
-	fmt.Println("Курс 0.9412€ EUR ⇄ 92₽ RUB =", eurRub, "RUB")
+	sum := enterSum()
+	fmt.Print(sum)
+}
+
+func enterSum() float64 {
+	var sum float64
+	fmt.Print("Введите сумму для обмена: ")
+	fmt.Scan(&sum)
+	return sum
+}
+
+func outputResult(sum float64, usdRub, usdEur float64) float64 {
+	return sum
 }
