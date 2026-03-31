0ac9f97bedfaa00b362b939450377579c3c9a8b4
Nazim Fataliev <nazim.rabota@bk.ru>
2026-03-31 10:56:54 +0300

    else if

--- main.go
+++ main.go
@@ -9,17 +9,24 @@ func main() {
 	fmt.Println("Калькулятор индекса массы тела")
 	userKG, userHeight := getUSerInput()
 	imt := calculateIMT(userKG, userHeight)
-	isLean := imt < 16
-	if isLean {
-		fmt.Println("у вас недостаток веса")
-	}
 	outputResult(imt)
+	if imt < 16 {
+		fmt.Println("у вас сильный дифецит массы тела")
+	} else if imt < 18.5 {
+		fmt.Println("у вас дифецит массы тела")
+	} else if imt < 25 {
+		fmt.Println("у вас нормальная  масса тела")
+	} else if imt < 30 {
+		fmt.Println("у вас избыточный вес")
+	} else {
+		fmt.Println("у вас степень ожирения")
+	}
 
 }
 
 func outputResult(IMT float64) {
 	result := fmt.Sprintf("Ваш индекс массы тела: %.0f", IMT)
-	fmt.Print(result)
+	fmt.Println(result)
 }
 
 func calculateIMT(userKG float64, userHeight float64) float64 {
